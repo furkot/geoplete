b31fb0deb064acfd9858a695f30b234da524762f
Natalia Kowalczyk <melitele@code42day.com>
2018-08-14 16:47:16 -0600

    skip query if enough entries match from the previous one

--- test/geoplete.js
+++ test/geoplete.js
@@ -25,4 +25,52 @@ describe('geoplete', function () {
 
     assert.ok(ac, 'should create autocomplete object');
   });
+
+  it('matching', function (done) {
+    var input = document.getElementById('test'), count = 0;
+    this.ac = geoplete(input, {
+      type: 'place',
+      geocoder: {
+        order: ['synchronous'],
+        synchronous_parameters: {
+          response: function (query) {
+            count += 1;
+            if (query.place === 'san franc') {
+              return [
+                { place: 'San Francisco, Ciudad Santa Catarina, Nuevo León, Mexico' },
+                { place: 'San Francisco, Agua Prieta, Sonora, Mexico' },
+                { place: 'San Francisco, Soledad de Graciano Sánchez, San Luis Potosí, Mexico' },
+                { place: 'San Francisco, Ciudad Melchor Múzquiz, Coahuila de Zaragoza, Mexico' },
+                { place: 'San Francisco, Mexico City, Ciudad de México, Mexico' },
+                { place: 'San Francisco, Tonalá, Chiapas, Mexico' },
+                { place: 'San Francisco, Sombrerete, Zacatecas, Mexico' },
+                { place: 'San Francisco, Antofagasta, De Antofagasta, Chile' },
+                { place: 'Fazenda Santa Francisca, QUERÃNCIA DO NORTE, Paraná, Brazil' },
+                { place: 'Sitio Santo Francisco, Catarina, Ceara, Brazil' }
+              ];
+            }
+            assert.fail();
+          }
+        },
+        synchronous_enable: function () { return true; }
+      }
+    });
+    input.value = 'san franc';
+    input.dispatchEvent(new Event('input', {
+      'bubbles': true,
+      'cancelable': true
+    }));
+    setTimeout(function () {
+      input.value = 'san franci';
+      input.dispatchEvent(new Event('input', {
+        'bubbles': true,
+        'cancelable': true
+      }));
+      setTimeout(function () {
+        assert.equal(count, 1);
+        done();
+      }, 500);
+    }, 500);
+  });
 });
+
